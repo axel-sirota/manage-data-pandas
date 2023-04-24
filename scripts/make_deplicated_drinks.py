8bcefc594fdc88e25116e95ede27b77c0cb6a7e1
Axel Sirota <axel.sirota@gmail.com>
2023-04-24 16:32:34 -0300

    add duplicated data

---
+++ scripts/make_deplicated_drinks.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import random
+
+drinks = pd.read_csv("../data/drinks.csv")
+new_df_list = []
+for row in drinks.iterrows():
+    if random.random() > 0.7:
+        number_of_duplicates = random.randint(2, 5)
+        for _ in range(number_of_duplicates):
+            new_df_list.append(list(row[1]))
+    else:
+        new_df_list.append(list(row[1]))
+new_df = pd.DataFrame(new_df_list, columns=drinks.columns)
+new_df.to_csv('../data/drinks_duplicated.csv')
